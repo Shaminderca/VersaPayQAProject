541d0c8ad17061dd38964fea54f5150043737536
Shaminderca <56711365+Shaminderca@users.noreply.github.com>
2020-10-03 18:00:45 -0400

    Manvir class modified (#7)
    Co-authored-by: Manvir <manvir.singh.parmar@gmail.com>

--- src/main/java/com/versapay/qa/pages/Manvir.java
+++ src/main/java/com/versapay/qa/pages/Manvir.java
@@ -5,7 +5,7 @@ public class Manvir {
 	public static void main(String[] args) {
 			
 
-      System.out.println("Manvir Class");
+      System.out.println("Manvir Class made at 5.15 pm at Brampton");
 		
 		
 	}
